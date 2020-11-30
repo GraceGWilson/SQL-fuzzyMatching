83e0220e7f5f5014d1d8743dd3f752540860f046
wil00303 <wil00303@umn.edu>
2020-11-30 03:05:17 -0600

    Add files via upload
    dist <= 3; -- threshold set to 3

--- challenge.sql
+++ challenge.sql
@@ -16,7 +16,7 @@
 -- You can uncomment this for testing, but leave it commented out
 -- when you submit your script. The system will set this variable to 
 -- various target words when scoring your query.
--- SET @word = 'pumkin';
+SET @word = 'Recieve';
 
 
 -- Here is a very basic approach (removing double m's) that returns
@@ -32,7 +32,7 @@
 		CASE WHEN misspelled_word = @word THEN 0
              -- WHEN c_se = s_se THEN 0
 			WHEN LEFT(dm(c_short),2) != LEFT(dm(s_short),2) THEN 200
-            WHEN RIGHT(c_se,1) != RIGHT(s_se,1) THEN 200
+            WHEN RIGHT(dm(c_short),1) != RIGHT(dm(s_short),1) THEN 200
             WHEN RIGHT(c_short,3) = RIGHT(s_short,3) THEN LEVENSHTEIN(misspelled_word,@word)
             WHEN RIGHT(c_short,2) = RIGHT(s_short,2) THEN LEVENSHTEIN(misspelled_word,@word)
             WHEN LEFT(c_short,4) = LEFT(s_short,4) THEN LEVENSHTEIN(misspelled_word,@word)
@@ -44,14 +44,14 @@
              -- WHEN dm(misspelled_word) = dm(@word) THEN LEVENSHTEIN(misspelled_word,@word)
 			 ELSE 400
 		 END AS dist
+         -- dm(c_short),
+		 -- dm(s_short)
  FROM(
   SELECT id, misspelled_word, 
-  SOUNDEX(misspelled_word) AS c_se, 
-  SOUNDEX(@word) AS s_se, 
+  -- SOUNDEX(misspelled_word) AS c_se, 
+  -- SOUNDEX(@word) AS s_se, 
   REGEXP_REPLACE(misspelled_word, '(ed|ing|s)$', '') AS c_short,
-  REGEXP_REPLACE(@word, '(ed|ing|s)$', '') AS s_short,
-  dm(misspelled_word),
-  dm(@word)
+  REGEXP_REPLACE(@word, '(ed|ing|s)$', '') AS s_short
    FROM word
    WHERE SUBSTR(SOUNDEX(misspelled_word),2,3) LIKE SUBSTR(SOUNDEX(@word),2,3)
       OR LEFT(@word,3) LIKE LEFT(misspelled_word,3)
@@ -59,7 +59,7 @@
       ) AS T
 )
 SELECT * FROM cte
-WHERE dist <= 3; -- CHAR_LENGTH(@word)*.3;
+WHERE dist <= 3;
 
 
 /*
