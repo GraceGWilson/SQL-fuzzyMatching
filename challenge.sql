2f740f01a46a34a0242766ba43eb94283a967255
wil00303 <wil00303@umn.edu>
2020-12-03 17:47:23 -0600

    bigram

--- challenge.sql
+++ challenge.sql
@@ -24,30 +24,30 @@
 -- 'pumpkin'
 
 SELECT id, misspelled_word 
-FROM ( SELECT *, SUBSTR(misspelled_word,1,3) AS mf1, SUBSTR(misspelled_word,2,3) AS mf2, 
-      		SUBSTR(misspelled_word,3,3) AS mf3, SUBSTR(misspelled_word,4,3) AS mf4, 
-      		SUBSTR(misspelled_word,4,3) AS mf5, SUBSTR(misspelled_word,6,3) AS mf6,
-                SUBSTR(misspelled_word,7,3) AS mf7, SUBSTR(misspelled_word,8,3) AS mf8,
-      		SUBSTR(REVERSE(misspelled_word),1,3) AS mr1, SUBSTR(REVERSE(misspelled_word),2,3) AS mr2,
-                SUBSTR(REVERSE(misspelled_word),3,3) AS mr3, SUBSTR(REVERSE(misspelled_word),4,3) AS mr4,
-                SUBSTR(REVERSE(misspelled_word),5,3) AS mr5, SUBSTR(REVERSE(misspelled_word),6,3) AS mr6, 
-      		SUBSTR(REVERSE(misspelled_word),6,3) AS mr7, SUBSTR(REVERSE(misspelled_word),8,3) AS mr8
+FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2) AS mf2, 
+      		SUBSTR(misspelled_word,3,2) AS mf3, SUBSTR(misspelled_word,4,2) AS mf4, 
+      		SUBSTR(misspelled_word,4,2) AS mf5, SUBSTR(misspelled_word,6,2) AS mf6,
+                SUBSTR(misspelled_word,7,2) AS mf7, SUBSTR(misspelled_word,8,2) AS mf8,
+      		SUBSTR(REVERSE(misspelled_word),1,2) AS mr1, SUBSTR(REVERSE(misspelled_word),2,2) AS mr2,
+                SUBSTR(REVERSE(misspelled_word),3,2) AS mr3, SUBSTR(REVERSE(misspelled_word),4,2) AS mr4,
+                SUBSTR(REVERSE(misspelled_word),5,2) AS mr5, SUBSTR(REVERSE(misspelled_word),6,2) AS mr6, 
+      		SUBSTR(REVERSE(misspelled_word),6,2) AS mr7, SUBSTR(REVERSE(misspelled_word),8,2) AS mr8
 	FROM word
 	HAVING @word SOUNDS LIKE misspelled_word OR
-		SUBSTR(@word,1,3) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
-		SUBSTR(@word,2,3) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
-		SUBSTR(@word,3,3) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
-		SUBSTR(@word,4,3) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
-		SUBSTR(@word,6,3) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
-		SUBSTR(@word,7,3) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
-      		SUBSTR(@word,8,3) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
-      		SUBSTR(REVERSE(@word),1,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
-      		SUBSTR(REVERSE(@word),2,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR 	
-		SUBSTR(REVERSE(@word),3,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
-		SUBSTR(REVERSE(@word),4,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
-		SUBSTR(REVERSE(@word),5,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
-		SUBSTR(REVERSE(@word),6,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
-		SUBSTR(REVERSE(@word),7,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
-      		SUBSTR(REVERSE(@word),8,3) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8)
+		SUBSTR(@word,1,2) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
+		SUBSTR(@word,2,2) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
+		SUBSTR(@word,3,2) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
+		SUBSTR(@word,4,2) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
+		SUBSTR(@word,6,2) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
+		SUBSTR(@word,7,2) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
+      		SUBSTR(@word,8,2) IN (mf1, mf2, mf3, mf4, mf5, mf6, mf7,mf8) OR
+      		SUBSTR(REVERSE(@word),1,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
+      		SUBSTR(REVERSE(@word),2,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR 	
+		SUBSTR(REVERSE(@word),3,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
+		SUBSTR(REVERSE(@word),4,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
+		SUBSTR(REVERSE(@word),5,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
+		SUBSTR(REVERSE(@word),6,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
+		SUBSTR(REVERSE(@word),7,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8) OR
+      		SUBSTR(REVERSE(@word),8,2) IN (mr1, mr2, mr3, mr4, mr5, mr6, mr7, mr8)
 	 ) AS t 
 WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld_ratio(@word, misspelled_word) >= 60); 
