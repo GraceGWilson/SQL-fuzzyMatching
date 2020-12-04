141de71ac7dc0a993a6ddeed2fa79b287040dd51
wil00303 <wil00303@umn.edu>
2020-12-03 18:53:53 -0600

    59

--- challenge.sql
+++ challenge.sql
@@ -39,23 +39,24 @@ FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2)
 	FROM word
 	HAVING @word SOUNDS LIKE misspelled_word OR
 		SUBSTR(@word,1,2) IN (mf1, mf2, mf3, mf4) OR
-		SUBSTR(@word,2,2) IN (mf1, mf2, mf3, mf4, mf5) OR
+		SUBSTR(@word,2,2) IN (mf1, mf2, mf3, mf4) OR
 		SUBSTR(@word,3,2) IN (mf1, mf2, mf3, mf4, mf5) OR
-		SUBSTR(@word,4,2) IN (mf3, mf4, mf5, mf6, mf7) OR
-		SUBSTR(@word,6,2) IN (mf4, mf5, mf6, mf7,mf8) OR
-		SUBSTR(@word,7,2) IN (mf5, mf6, mf7,mf8,mf9) OR
+		SUBSTR(@word,4,2) IN (mf2, mf3, mf4, mf5, mf6, mf7) OR
+     		SUBSTR(@word,5,2) IN (mf3, mf4, mf5, mf6, mf7) OR
+		SUBSTR(@word,6,2) IN (mf4, mf5, mf6, mf7, mf8) OR
+		SUBSTR(@word,7,2) IN (mf5, mf6, mf7, mf8, mf9) OR
       		SUBSTR(@word,8,2) IN (mf6, mf7,mf8,mf9,mf10) OR
       		SUBSTR(@word,9,2) IN (mf7,mf8,mf9,mf10,mf11) OR
       		SUBSTR(@word,10,2) IN (mf8,mf9,mf10,mf11,mf12) OR
-      		SUBSTR(REVERSE(@word),1,3) IN (mr1, mr2, mr3, mr4, mr5) OR
-      		SUBSTR(REVERSE(@word),2,3) IN (mr1, mr2, mr3, mr4, mr5) OR 	
-		SUBSTR(REVERSE(@word),3,3) IN (mr1, mr2, mr3, mr4, mr5) OR
-		SUBSTR(REVERSE(@word),4,3) IN (mr2, mr3, mr4, mr5, mr6) OR
-		SUBSTR(REVERSE(@word),5,3) IN (mr3, mr4, mr5, mr6, mr7) OR
-		SUBSTR(REVERSE(@word),6,3) IN (mr4, mr5, mr6, mr7, mr8) OR
-		SUBSTR(REVERSE(@word),7,3) IN (mr5, mr6, mr7, mr8, mr9) OR
-      		SUBSTR(REVERSE(@word),8,3) IN (mr5, mr6, mr7, mr8, mr9) OR
-      		SUBSTR(REVERSE(@word),9,3) IN (mr7, mr8, mr9, mr10, mr11) OR
-      		SUBSTR(REVERSE(@word),10,3) IN (mr8, mr9, mr10, mr11, mr12)
+      		SUBSTR(REVERSE(@word),1,2) IN (mr1, mr2, mr3,, mr4) OR
+      		SUBSTR(REVERSE(@word),2,2) IN (mr1, mr2, mr3, mr4) OR 	
+		SUBSTR(REVERSE(@word),3,2) IN (mr1, mr2, mr3, mr4, mr5) OR
+		SUBSTR(REVERSE(@word),4,2) IN (mr2, mr3, mr4, mr5, mr6) OR
+		SUBSTR(REVERSE(@word),5,2) IN (mr3, mr4, mr5, mr6, mr7) OR
+		SUBSTR(REVERSE(@word),6,2) IN (mr4, mr5, mr6, mr7, mr8) OR
+		SUBSTR(REVERSE(@word),7,2) IN (mr5, mr6, mr7, mr8, mr9) OR
+      		SUBSTR(REVERSE(@word),8,2) IN (mr5, mr6, mr7, mr8, mr9) OR
+      		SUBSTR(REVERSE(@word),9,2) IN (mr7, mr8, mr9, mr10, mr11) OR
+      		SUBSTR(REVERSE(@word),10,2) IN (mr8, mr9, mr10, mr11, mr12)
 	 ) AS t 
-WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld_ratio(@word, misspelled_word) >= 58); 
+WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld_ratio(@word, misspelled_word) >= 59); 
