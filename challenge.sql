eb75fa3a7919ac131c49d2e40d7ed04a72a159ea
wil00303 <wil00303@umn.edu>
2020-12-04 00:15:38 -0600

    69,50

--- challenge.sql
+++ challenge.sql
@@ -35,8 +35,8 @@ FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2)
                 SUBSTR(REVERSE(misspelled_word),3,2) AS mr3, SUBSTR(REVERSE(misspelled_word),4,2) AS mr4,
                 SUBSTR(REVERSE(misspelled_word),5,2) AS mr5, SUBSTR(REVERSE(misspelled_word),6,2) AS mr6, 
       		SUBSTR(REVERSE(misspelled_word),7,2) AS mr7, SUBSTR(REVERSE(misspelled_word),8,2) AS mr8,
-      		SUBSTR(REVERSE(misspelled_word),9,2) AS mr9, SUBSTR(REVERSE(misspelled_word),10,2) AS mr10
-      		-- SUBSTR(REVERSE(misspelled_word),11,2) AS mr11,SUBSTR(REVERSE(misspelled_word),12,2) AS mr12
+      		SUBSTR(REVERSE(misspelled_word),9,2) AS mr9, SUBSTR(REVERSE(misspelled_word),10,2) AS mr10,
+      	        SUBSTR(REVERSE(misspelled_word),11,2) AS mr11,SUBSTR(REVERSE(misspelled_word),12,2) AS mr12
 	FROM word
 	HAVING -- dm(@word) LIKE dm(misspelled_word) -- OR
 		 SUBSTR(@word,1,2) IN (mf1, mf2, mf3) OR
@@ -54,11 +54,11 @@ FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2)
 		 SUBSTR(REVERSE(@word),5,2) IN (mr3, mr4, mr5, mr6, mr7) OR
 		 SUBSTR(REVERSE(@word),6,2) IN (mr4, mr5, mr6, mr7, mr8) OR
 		 SUBSTR(REVERSE(@word),7,2) IN (mr5, mr6, mr7, mr8, mr9) OR
-      		 SUBSTR(REVERSE(@word),8,2) IN (mr6, mr7, mr8, mr9, mr10) 
-      		-- SUBSTR(REVERSE(@word),9,2) IN (mr6, mr8, mr9, mr10, mr11) 
-      		-- SUBSTR(REVERSE(@word),10,2) IN (mr8, mr9, mr10, mr11, mr12)
+      		 SUBSTR(REVERSE(@word),8,2) IN (mr6, mr7, mr8, mr9, mr10) OR
+      		 SUBSTR(REVERSE(@word),9,2) IN (mr6, mr8, mr9, mr10, mr11) OR
+      		 SUBSTR(REVERSE(@word),10,2) IN (mr8, mr9, mr10, mr11, mr12)
 	 ) AS t 
-WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld_ratio(SOUNDEX(@word), SOUNDEX(misspelled_word)) >= 67)
+WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld_ratio(SOUNDEX(@word), SOUNDEX(misspelled_word)) >= 69)
 )
 SELECT * 
 FROM CTE
