1a17fda1a8d714ffdd501642959c7a8d3148cb67
wil00303 <wil00303@umn.edu>
2020-12-04 02:27:39 -0600

    71 and expanded search

--- challenge.sql
+++ challenge.sql
@@ -38,7 +38,7 @@ FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2)
       		SUBSTR(REVERSE(misspelled_word),9,2) AS mr9, SUBSTR(REVERSE(misspelled_word),10,2) AS mr10,
       	        SUBSTR(REVERSE(misspelled_word),11,2) AS mr11,SUBSTR(REVERSE(misspelled_word),12,2) AS mr12
 	FROM word
-	HAVING -- dm(@word) LIKE dm(misspelled_word) -- OR
+	HAVING   @word SOUNDS LIKE misspelled_word OR
 		 SUBSTR(@word,1,2) IN (mf1, mf2, mf3) OR
 		 SUBSTR(@word,2,2) IN (mf1, mf2, mf3, mf4) OR
 		 SUBSTR(@word,3,2) IN (mf1, mf2, mf3, mf4, mf5) OR
@@ -55,8 +55,8 @@ FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2)
 		 SUBSTR(REVERSE(@word),6,2) IN (mr4, mr5, mr6, mr7, mr8) OR
 		 SUBSTR(REVERSE(@word),7,2) IN (mr5, mr6, mr7, mr8, mr9) OR
       		 SUBSTR(REVERSE(@word),8,2) IN (mr6, mr7, mr8, mr9, mr10) OR
-      		 SUBSTR(REVERSE(@word),9,2) IN (mr6, mr8, mr9, mr10, mr11) 
-      		 -- SUBSTR(REVERSE(@word),10,2) IN (mr8, mr9, mr10, mr11, mr12)
+      		 SUBSTR(REVERSE(@word),9,2) IN (mr6, mr8, mr9, mr10, mr11) OR
+      		 SUBSTR(REVERSE(@word),10,2) IN (mr8, mr9, mr10, mr11, mr12)
 	 ) AS t 
 WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld_ratio(SOUNDEX(@word), SOUNDEX(misspelled_word)) >= 71)
 )
