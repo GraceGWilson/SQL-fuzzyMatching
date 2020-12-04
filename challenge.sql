638ef25362e4aec4b62d5e336ef66ddd9383ca3c
wil00303 <wil00303@umn.edu>
2020-12-04 16:10:52 -0600

    ratio >=65 ld <5

--- challenge.sql
+++ challenge.sql
@@ -25,18 +25,12 @@
 WITH CTE AS
 (
 SELECT id, misspelled_word 
-FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2) AS mf2, 
-      		SUBSTR(misspelled_word,3,2) AS mf3, SUBSTR(misspelled_word,4,2) AS mf4, 
-      		SUBSTR(misspelled_word,4,2) AS mf5, SUBSTR(misspelled_word,6,2) AS mf6,
-                SUBSTR(misspelled_word,7,2) AS mf7, SUBSTR(misspelled_word,8,2) AS mf8, 
-      		SUBSTR(misspelled_word,9,2) AS mf9, SUBSTR(misspelled_word,10,2) AS mf10,
-      		SUBSTR(misspelled_word,11,2) AS mf11, SUBSTR(misspelled_word,12,2) AS mf12,
-      		SUBSTR(REVERSE(misspelled_word),1,2) AS mr1, SUBSTR(REVERSE(misspelled_word),2,2) AS mr2,
-                SUBSTR(REVERSE(misspelled_word),3,2) AS mr3, SUBSTR(REVERSE(misspelled_word),4,2) AS mr4,
-                SUBSTR(REVERSE(misspelled_word),5,2) AS mr5, SUBSTR(REVERSE(misspelled_word),6,2) AS mr6, 
-      		SUBSTR(REVERSE(misspelled_word),7,2) AS mr7, SUBSTR(REVERSE(misspelled_word),8,2) AS mr8,
-      		SUBSTR(REVERSE(misspelled_word),9,2) AS mr9, SUBSTR(REVERSE(misspelled_word),10,2) AS mr10
-      	       -- SUBSTR(REVERSE(misspelled_word),11,2) AS mr11,SUBSTR(REVERSE(misspelled_word),12,2) AS mr12
+FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2) AS mf2, SUBSTR(misspelled_word,3,2) AS mf3, 
+      		SUBSTR(misspelled_word,4,2) AS mf4, SUBSTR(misspelled_word,4,2) AS mf5, SUBSTR(misspelled_word,6,2) AS mf6,
+                SUBSTR(misspelled_word,7,2) AS mf7, SUBSTR(misspelled_word,8,2) AS mf8, SUBSTR(misspelled_word,9,2) AS mf9,
+      		SUBSTR(REVERSE(misspelled_word),1,2) AS mr1, SUBSTR(REVERSE(misspelled_word),2,2) AS mr2, SUBSTR(REVERSE(misspelled_word),3,2) AS mr3, 
+      		SUBSTR(REVERSE(misspelled_word),4,2) AS mr4, SUBSTR(REVERSE(misspelled_word),5,2) AS mr5, SUBSTR(REVERSE(misspelled_word),6,2) AS mr6, 
+      		SUBSTR(REVERSE(misspelled_word),7,2) AS mr7, SUBSTR(REVERSE(misspelled_word),8,2) AS mr8, SUBSTR(REVERSE(misspelled_word),9,2) AS mr9
 	FROM word
 	HAVING  -- @word SOUNDS LIKE misspelled_word OR
 		 SUBSTR(@word,1,2) IN (mf1, mf2, mf3) OR
@@ -46,21 +40,17 @@ FROM ( SELECT *, SUBSTR(misspelled_word,1,2) AS mf1, SUBSTR(misspelled_word,2,2)
      		 SUBSTR(@word,5,2) IN (mf3, mf4, mf5, mf6, mf7) OR
 		 SUBSTR(@word,6,2) IN (mf4, mf5, mf6, mf7, mf8) OR
 		 SUBSTR(@word,7,2) IN (mf5, mf6, mf7, mf8, mf9) OR
-      		 SUBSTR(@word,8,2) IN (mf6, mf7,mf8,mf9,mf10)  OR
       		 SUBSTR(REVERSE(@word),1,2) IN (mr1, mr2, mr3) OR
       		 SUBSTR(REVERSE(@word),2,2) IN (mr1, mr2, mr3, mr4) OR 	
 		 SUBSTR(REVERSE(@word),3,2) IN (mr1, mr2, mr3, mr4, mr5) OR
 		 SUBSTR(REVERSE(@word),4,2) IN (mr2, mr3, mr4, mr5, mr6) OR
 		 SUBSTR(REVERSE(@word),5,2) IN (mr3, mr4, mr5, mr6, mr7) OR
 		 SUBSTR(REVERSE(@word),6,2) IN (mr4, mr5, mr6, mr7, mr8) OR
-		 SUBSTR(REVERSE(@word),7,2) IN (mr5, mr6, mr7, mr8, mr9) OR
-      		 SUBSTR(REVERSE(@word),8,2) IN (mr6, mr7, mr8, mr9, mr10) 
-      		-- SUBSTR(REVERSE(@word),9,2) IN (mr6, mr8, mr9, mr10, mr11) OR
-      		 -- SUBSTR(REVERSE(@word),10,2) IN (mr8, mr9, mr10, mr11, mr12)
+		 SUBSTR(REVERSE(@word),7,2) IN (mr5, mr6, mr7, mr8, mr9) 
 	 ) AS t 
-WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld(SOUNDEX(@word), SOUNDEX(misspelled_word)) < 5)
+WHERE EXISTS (SELECT id FROM word as w where w.id = t.id AND ld_ratio(SOUNDEX(@word), SOUNDEX(misspelled_word)) >= 65)
 )
 SELECT * 
 FROM CTE
-WHERE EXISTS (SELECT id FROM word as w where w.id = CTE.id AND (ld_ratio(@word, misspelled_word) >= 60); 
+WHERE EXISTS (SELECT id FROM word as w where w.id = CTE.id AND ld(@word, misspelled_word) < 5; 
 									      
